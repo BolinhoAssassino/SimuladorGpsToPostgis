fdc6f79442485c4e1285b4830921cd5583d860d3
Yuri guilherme <39995851+BolinhoAssassino@users.noreply.github.com>
2019-10-01 17:56:29 -0400

    Add files via upload

---
+++ python_boladao.py
@@ -0,0 +1,53 @@
+import shapefile
+import time
+from django.contrib.gis.geos import GEOSGeometry
+import psycopg2
+import datetime
+#conexao locals
+shapefile_name = input('shp file name: ')
+host = input('host: ')
+user = input('user: ')
+password = input('password: ')
+loop = input('loop ? 1 = True : ')
+
+try:
+    conexão = psycopg2.connect("dbname=financas_dejango user={} password={} host={}".format(user,password,host))
+except:
+    print('Fatal error \n tente novamente com uma senha diferente')
+
+def abrirShape():
+    shp = shapefile.Reader(shapefile_name)
+    gps  = shp.shapeRecords()
+    sequencia_pontos = gps
+    return sequencia_pontos 
+def execute_commit(squel, cursor):
+    cursor.execute(squel)
+    global conexão
+    conexão.commit()
+
+
+def verificar(ponto, cursor):
+    depois_antes = datetime.datetime.now()
+    limite = 0
+    agora = datetime.datetime.now()
+    GEOgpsPonto = GEOSGeometry("POINT({} {})".format(str(ponto[0]), str(ponto[1])),srid=4326)
+    execute_commit("UPDATE malha_viaria.onibus set geom = ST_GeomFromEWKT('{}') where id = 1".format(GEOgpsPonto),cursor)
+    time.sleep(10)
+    print('')
+def serie_pontos():
+    retorno_sql =  abrirShape()
+    c = 0
+    for registros in retorno_sql:
+        registros = registros.shape.points[0]
+        cursor = conexão.cursor()
+        c= verificar(registros, cursor)
+        cursor.close()
+        print('Passando para o próximo ponto. ')
+    return 'ok'
+
+if loop == '1':
+    while True:
+        serie_pontos()
+
+serie_pontos()
+conexão.close()
